b9e15b22d9db9d702c63b1c2a3129d2f17ec965a
Piyush <itsmepiyush267@gmail.com>
2024-11-01 18:06:33 +0530

    my name

---
+++ style.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a[] = "Piyush"; // or we use char a[20]="Piyush" and we use '' these for single characters and "" for strings
+    cout << a;
+    return 0;
+}
